0a4547a4e4e26c2819b830a0ace55e57a0fbc42f
Alexandr <167582875+AlexKhorosh97@users.noreply.github.com>
2024-10-31 05:30:46 +0400

    lesson_03 directory

--- lesson_03/smartphone.py
+++
@@ -1,6 +0,0 @@
-class Smartphone:
-
-    def __init__(self, phone_brand, model, phone_number):
-        self.phone_brand = phone_brand
-        self.model = model
-        self.phone_number = phone_number
